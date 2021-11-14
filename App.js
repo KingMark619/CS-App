32a85fdad19f08f821c02187aafdac55a7314db5
King Mark <marcokayumbam@icloud.com>
2021-11-14 15:16:22 +0800

    FirstUploadProjectSetup

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+const handleSignup = () => {
+  const email = 'hello@234.com'
+  const password = 'jeez5545'
+  // .createUserWithEmailAndPassword(email, password)
+  // .then((userCredentials) => {
+  //   const user = userCredentials.user
+  //   console.log(user.email)
+  // })
+  // .catch((error) => {alert(error.message)})
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button  title='log' onPress={handleSignup}/>
     </View>
   );
 }
---
+++ firebase.js
@@ -0,0 +1,34 @@
+// import { initializeApp } from 'firebase/app';
+// import * as firebase from 'firebase'
+// // import * as firebase from 'firebase';
+// // Optionally import the services that you want to use
+// // import { auth } from "firebase/auth";
+// //import {...} from "firebase/database";
+// //import {...} from "firebase/firestore";
+// //import {...} from "firebase/functions";
+// //import {...} from "firebase/storage";
+
+// // Initialize Firebase
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyAhlGHogeMSl0vpeSbp9aN7Z9tm_YnRX4I',
+//   authDomain: 'congo-sante-51130.firebaseapp.com',
+//   databaseURL: 'https://congo-sante-51130.firebaseio.com',
+//   projectId: 'congo-sante-51130',
+//   storageBucket: 'gs://congo-sante-51130.appspot.com',
+//   messagingSenderId: '676690535391',
+//   appId: '1:676690535391:ios:fa82b4322d07b7daf39a6a',
+// //   measurementId: 'G-measurement-id',
+// };
+
+// // initializeApp (firebaseConfig);
+
+// let app;
+// if (firebase.apps.lenght === 0) {
+//   app = initializeApp(firebaseConfig)
+// } else {
+//   app= firebase.app()
+// }
+
+// const auth = firebase.auth()
+
+// export { auth }
